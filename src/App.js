467cbd9b5443cba9a96647ba2a56805a318b1444
santhosh-ri <126945769+santhosh-ri@users.noreply.github.com>
2023-03-05 22:51:40 +0530

    APP-SETUP

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        PRODUCTIVITY LOGGER
+      </header>
+    </div>
+  );
+}
+
+export default App;
